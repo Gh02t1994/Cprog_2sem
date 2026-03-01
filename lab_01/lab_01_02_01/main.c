fb9ea257541356e65a96f21e22eb1438c5f26e9b
byav25u214 <balyabinyav@student.bmstu.ru>
2026-03-01 17:48:12 +0300

    Add tasks 1 - 4 with tests

---
+++ lab_01/lab_01_02_01/main.c
@@ -0,0 +1,50 @@
+/**
+ * @file main.c
+ * @author Balyabin Yaroslav (balyabinyav@student.bmstu.ru)
+ * @brief Lab 01 02 01
+ * @version 0.1
+ * @date 2026-02-27
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+/** The number of pi */
+#define PI 3.14159265
+
+/**
+ * @brief calculating the area of an isosceles trapezoid by bases and angle with a larger base
+ * 
+ * @return int
+ * 0 — successful completion
+ * 1 — input error
+ * 2 — incorrect mathematical data
+ */
+int main(void)
+{
+    float side_a, side_b, angle_phi; //**< trapezoid bases and angle */
+    float square; //**< trapezoid area */
+    float phi_rad; //**< angle in radians */
+
+    printf("Enter the length of the base a, b and the angle phi: ");
+    if (scanf("%f %f %f", &side_a, &side_b, &angle_phi) != 3) //**< validation for input correctness */
+    {
+        printf("Incorrect input\n");
+        return 1;
+    }
+
+    if (side_a <= 0 || side_b <= 0 || angle_phi <= 0 || angle_phi >= 180) //**< validation for mathematical correctness */
+    {
+        printf("Incorrect mathematical input\n");
+        return 2;
+    }
+
+    phi_rad = angle_phi * PI / 180.0; //**< convert angle from degrees to radians */
+    square = fabs(side_a * side_a - side_b * side_b) * tan(phi_rad) / 4; //**< calculation of the trapezoid area */
+
+    printf("The area of the trapezoid S: %.6f\n", square);
+    return 0;
+}
---
+++ lab_01/lab_01_04_01/main.c
@@ -0,0 +1,45 @@
+/**
+ * @file main.c
+ * @author Balyabin Yaroslav (balyabinyav@student.bmstu.ru)
+ * @brief Lab 01 04 01
+ * @version 0.1
+ * @date 2026-01-01
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+
+#include <stdio.h>
+
+/**
+ * @brief calculating the number of apartments and floor by number of flat
+ * 
+ * @return int
+ * 0 — successful completion
+ * 1 — input error
+ * 2 — incorrect mathematical data
+ */
+int main(void)
+{
+    int num_of_flat; //**< the number of flat */
+    int num_apart, floor; //**< the number of apartments and number of floor */
+
+    printf("Enter the number of flat: ");
+    if (scanf("%d", &num_of_flat) != 1) //**< validation for input correctness */
+    {
+        printf("Incorrect input");
+        return 1;
+    }
+
+    if (num_of_flat <= 0) //**< validation for mathematical correctness */
+    {
+        printf("Incorrect mathematical input");
+        return 2;
+    }
+
+    num_apart = (num_of_flat - 1) / 36 + 1; //**< calculating the number of apartments */
+    floor = ((num_of_flat - 1) % 36) / 4 + 1; //**< calculating the number of floor*/
+    
+    printf("The number of apartments and floor: %d %d", num_apart, floor);
+    return 0;
+}
